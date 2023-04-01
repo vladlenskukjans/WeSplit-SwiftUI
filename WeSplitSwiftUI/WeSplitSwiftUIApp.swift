6a7696d40885758007149d28a0d3be4ed0e55989
Vladlens Kukjans <kukjanvladlen@gmail.com>
2023-04-01 18:36:17 +0100

    Initial Commit

---
+++ WeSplitSwiftUI/WeSplitSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitSwiftUIApp.swift
+//  WeSplitSwiftUI
+//
+//  Created by Vladlens Kukjans on 01/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
